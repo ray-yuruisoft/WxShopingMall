3cc6a5fd647777b5ddac7d7f63c65bd90ccfa50a
ray-yuruisoft <417853832@qq.com>
2017-08-12 14:42:17 +0800

    shoppingCartIsDone
    购物车功能基本实现

--- Yuruisoft.ShoppingMall.Wx/Page/shoppingCart/shoppingCart.js
+++ Yuruisoft.ShoppingMall.Wx/Page/shoppingCart/shoppingCart.js
@@ -4,6 +4,7 @@ var shoppingCartSave = function (shoppingCart) {
   app.globalData.shoppingCart = shoppingCart;
   wx.setStorageSync('shoppingCart', shoppingCart);
 };
+var currentForInput = {};
 Page({
 
   /**
@@ -29,12 +30,15 @@ Page({
         itemBottom.choosedFlag = chooseStatue;
       })
     });
+
+
+    shoppingCart = this.checkAllFee(shoppingCart);
     this.setData({
       chooseAllOn: chooseStatue,
       shoppingCart: shoppingCart
     })
     app.globalData.shoppingCart = shoppingCart;
-
+    shoppingCartSave(shoppingCart);
   },
   navigation: function (e) {
     var id = e.currentTarget.id;
@@ -44,11 +48,13 @@ Page({
   },
 
   /* 数量选择组件 开始*/
-  doChooseNum: function (id, merchantId, price, operNum) {
+  doChooseNum: function (id, merchantId, price, operNum) {//商品加减处理函数
     this.doProduceChoose(id, merchantId);//强制为选中状态，并且选中数目要加上
 
-    //分三层处理
     var shoppingCart = this.data.shoppingCart;
+    this.checkAllFee(shoppingCart);//强制选择会引发没有check项目，先check一下
+    //分三层处理，原理和checkAllFee是一样的，只是增加了allItemCount字段
+
     //第一层处理
     shoppingCart.allItemCount += operNum;
     shoppingCart.chooseItemCount += operNum;
@@ -93,25 +99,30 @@ Page({
     var price = e.currentTarget.dataset.price;
     this.doChooseNum(id, merchantId, price, -1);
   },
-  inputChooseNum: function (e) {//TODO未设置
-    if (e.detail.value == 0)
-      return;
-    var temp = parseInt(e.detail.value);
-    this.setData({
-      chooseNum: temp
-    })
+  inputChooseNum: function (e) {//保存原有值
+    currentForInput = {
+      id: e.currentTarget.id,
+      merchantId: e.currentTarget.merchantid,
+      value: e.detail.value
+    }
   },
-  inputChooseNumBlur: function (e) {
-    if (e.detail.value == 0)
-      this.setData({
-        chooseNum: 1
-      })
-  },
-  nothingToDo: function (e) {
+  inputChooseNumBlur: function (e) {//input失去焦点触发
     console.log(e);
+    var input = (e.detail.value == 0 || e.detail.value == '') ? 1 : parseInt(e.detail.value);
+    var originValue = currentForInput.value;
+    var id = e.currentTarget.id;
+    var merchantId = e.currentTarget.dataset.merchantid;
+    var price = e.currentTarget.dataset.price;
+
+    this.doChooseNum(id, merchantId, price, input - originValue);
+
+  },
+  nothingToDo: function (e) {//避免点击向导航传递
   },
   /*数量选择组件 结束*/
-  merchantChoose: function (e) {
+
+
+  merchantChoose: function (e) {//商户选中入口
 
     var merchantId = e.currentTarget.id;
     var chooseItemState;
@@ -128,12 +139,15 @@ Page({
 
     this.doChooseAll(chooseItemState);
     shoppingCart.choosedFlag = this.data.chooseAllOn;
+
+    shoppingCart = this.checkAllFee(shoppingCart);//结算所有费用和选中项
+
     this.setData({
       shoppingCart: shoppingCart
     })
     shoppingCartSave(shoppingCart);
   },
-  produceChoose: function (e) {
+  produceChoose: function (e) {//商品选中入口
     var merchantId = e.currentTarget.dataset.merchantid;
     var id = e.currentTarget.id;
     var shoppingCart = this.data.shoppingCart;
@@ -168,13 +182,45 @@ Page({
     })
     this.doChooseAll(chooseItemState);
     shoppingCart.choosedFlag = this.data.chooseAllOn;
+    shoppingCart = this.checkAllFee(shoppingCart);//结算所有费用和选中项
     this.setData({
       shoppingCart: shoppingCart
     })
     shoppingCartSave(shoppingCart);
   },
+  checkAllFee: function (shoppingCart) {//结算购物车内所有选中项目
+    var oneLevelFeeSum = 0;
+    var oneLevelChooseItemCount = 0;
 
-  doProduceChoose: function (id, merchantId) {
+    var twoLevelFeeSum;
+    var twoLevelChooseItemCount;
+
+    shoppingCart.detail.forEach(item => {
+
+      twoLevelFeeSum = 0;//初始化
+      twoLevelChooseItemCount = 0;
+
+      item.produceArr.forEach(itemBottom => {
+        if (itemBottom.choosedFlag) {
+          itemBottom.feeSum = app.com.mul(parseInt(itemBottom.itemCount), parseFloat(itemBottom.price)).toFixed(2);
+          twoLevelChooseItemCount += itemBottom.itemCount;
+          twoLevelFeeSum = app.com.add(twoLevelFeeSum, itemBottom.feeSum).toFixed(2);
+        }
+      })
+
+      item.chooseItemCount = twoLevelChooseItemCount;//赋值保存
+      item.feeSum = twoLevelFeeSum;
+
+      oneLevelFeeSum = app.com.add(oneLevelFeeSum, twoLevelFeeSum).toFixed(2);
+      oneLevelChooseItemCount += twoLevelChooseItemCount;
+    })
+
+    shoppingCart.chooseItemCount = oneLevelChooseItemCount;
+    shoppingCart.feeSum = oneLevelFeeSum;
+
+    return shoppingCart;
+  },
+  doProduceChoose: function (id, merchantId) {//强制为选中状态，并且选中数目要加上
     var shoppingCart = this.data.shoppingCart;
     var chooseItemState;
     shoppingCart.detail.forEach(item => {
@@ -207,8 +253,7 @@ Page({
     })
     shoppingCartSave(shoppingCart);
   },
-
-  doChooseAll: function (itemState) {
+  doChooseAll: function (itemState) {//检查是否能全选
     var chooseAllOn = this.data.chooseAllOn;
     var shoppingCart = this.data.shoppingCart;
     var testFlag = true;
@@ -237,6 +282,7 @@ Page({
       }
     }
   },
+
 
   /**
    * 生命周期函数--监听页面加载
