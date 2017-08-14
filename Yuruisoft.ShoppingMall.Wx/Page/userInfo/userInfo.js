ebf0c6f5e523ca51a520b00e444f18d9669eb0d0
ray-yuruisoft <417853832@qq.com>
2017-08-14 22:22:19 +0800

    modify
    加入了微信客服

--- Yuruisoft.ShoppingMall.Wx/Page/userInfo/userInfo.js
+++ Yuruisoft.ShoppingMall.Wx/Page/userInfo/userInfo.js
@@ -5,39 +5,87 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {  
+  data: {
     userInfo: {},
+    bannerListArr: [{
+      id: 1,
+      iconName: "icon-weixin",
+      icontitle: "微信在线",
+      badgeNum: 9
+    }, {
+      id: 2,
+      iconName: "icon-mobile",
+      icontitle: "拨打电话",
+      badgeNum: 1
+    },
+    {
+      id: 3,
+      iconName: "icon-envelope",
+      icontitle: "发送邮件",
+      badgeNum: 9
+    }, {
+      id: 4,
+      iconName: "icon-ellipsis",
+      icontitle: "其他服务",
+      badgeNum: 0
+    }],
+
     listTypeOne: {
       listArr: [{
-        id:1,
+        id: 1,
         title: "我的订单",
         checkName: "查看全部订单",
         listArr: [{
           id: 1,
           iconName: "icon-credit-card-alt",
-          icontitle: "待付款"
-        },{
-          id:2,
-          iconName:"icon-truck",
-          icontitle: "待收货"
+          icontitle: "待付款",
+          badgeNum: 10
+        }, {
+          id: 2,
+          iconName: "icon-truck",
+          icontitle: "待收货",
+          badgeNum: 1
         },
         {
-          id:3,
-          iconName:"icon-comments-o",
-          icontitle:"待评价"
-        },{
-          id:4,
-          iconName:"icon-undo",
-          icontitle:"退换/售后"
+          id: 3,
+          iconName: "icon-comments-o",
+          icontitle: "待评价",
+          badgeNum: 9
+        }, {
+          id: 4,
+          iconName: "icon-wrench",
+          icontitle: "退换/售后",
+          badgeNum: 0
         }]
       }]
     },
+    listTypeTwo: {
+      title: "我的关注",
+      listArr: [{
+        id: 1,
+        title: "商品收藏",
+        count: 0
+      }, {
+        id: 2,
+        title: "店铺收藏",
+        count: 1
+      },
+      {
+        id: 3,
+        title: "搭配收藏",
+        count: 0
+      }, {
+        id: 4,
+        title: "我的足迹",
+        count: 10
+      }]
 
+    },
     hanSpace: '\r\n\r\n\r\n\r\n',//空格输出
     space: '\r\n',
   },
 
-
+  listTypeTwoItemTap: function (e) { },
   listTypeOneTap: function (e) {
 
     console.log(e);
